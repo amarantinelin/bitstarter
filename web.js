4204395cb9b88ec9825439c61992bfe054c1de12
Lin He <lhe3@binghamton.edu>
2013-09-11 00:35:22 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,9 +5,9 @@ app.use(express.logger());
 var fs = require('fs');
 var htmlfile = "index.html";
 app.get('/', function(request, response) {
-//    var data = fs.readFileSync(htmlfile).toString();
+    var data = fs.readFileSync(htmlfile).toString();
  //   var buf = new Buffer(data, 'utf-8');
-    response.send('Hello!');
+    response.send(html);
 });
 
 var port = process.env.PORT || 8080;

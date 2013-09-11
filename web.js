657632f1b7be1d473f8ab74949afc7ebb084c556
Lin He <lhe3@binghamton.edu>
2013-09-11 00:34:26 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var htmlfile = "index.html";
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//    var data = fs.readFileSync(htmlfile).toString();
+ //   var buf = new Buffer(data, 'utf-8');
+    response.send('Hello!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
